7eda11a943545059d22a0a3770676fbeb819ba91
user <user@Sandbox.myguest.virtualbox.org>
2026-04-10 11:01:35 +0200

    fix: fix corrupt local repo

--- p3/scripts/remove-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-sudo rm -f /etc/apt/sources.list.d/docker.list    
-sudo rm -f /etc/apt/keyrings/docker.gpg 
-sudo apt remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras docker-buildx-plugin
-sudo rm -rf /var/lib/docker
